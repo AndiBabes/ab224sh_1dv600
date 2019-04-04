e0e89fa8bee915ed2288413bd6cbfad6f35438d3
Andi <ab224sh@student.lnu.se>
2019-04-04 16:36:24 +0200

    I added the new iteration of the game and the use cases

---
+++ SourceCode/GameMain.java
@@ -0,0 +1,97 @@
+package hangman;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GameMain extends Application
+{
+
+	public void start(Stage primaryStage)
+	{
+		Button playButton = new Button("Play game");
+		Button highscores = new Button("view highscores");
+		Button quit = new Button("quit game");
+		VBox MMRoot = new VBox();
+		MMRoot.setAlignment(Pos.CENTER);
+		MMRoot.setPadding(new Insets(5));
+		MMRoot.setSpacing(10);
+		MMRoot.getChildren().addAll(playButton, highscores, quit);
+		Scene menuScene = new Scene(MMRoot, 640, 480);
+
+		GridPane PGRoot = new GridPane();
+		Play play = new Play();
+		Label word = new Label(play.getWord());
+		PGRoot.add(word, 0, 0);
+		Label mistakes = new Label(play.getMistakes() + " mistakes");
+		PGRoot.add(mistakes, 1, 0);
+		PGRoot.add(new Label("provide a letter"), 0, 1);
+		TextField letter = new TextField();
+		PGRoot.add(letter, 0, 2);
+		Button checkLetter = new Button("Check Letter");
+		PGRoot.add(checkLetter, 1, 2);
+		Scene playScene = new Scene(PGRoot, 640, 480);
+
+		VBox GORoot = new VBox();
+		HBox gOBox = new HBox();
+		Button playAgain=new Button("Play again");
+		Button backToMenu = new Button("back to main menu");
+		gOBox.getChildren().addAll(playAgain,backToMenu);
+		gOBox.setAlignment(Pos.CENTER);
+		gOBox.setSpacing(10);
+		GORoot.setAlignment(Pos.CENTER);
+		GORoot.setPadding(new Insets(5));
+		GORoot.setSpacing(5);
+		Label message = new Label();
+		GORoot.getChildren().addAll(message, gOBox);
+		Scene gameOver = new Scene(GORoot, 640, 480);
+		playAgain.setOnAction(e->{
+			primaryStage.setScene(playScene);
+		});
+		backToMenu.setOnAction(e -> {
+			primaryStage.setScene(menuScene);
+		});
+		checkLetter.setOnAction(e -> {
+			play.play(letter.getText().toLowerCase().charAt(0));
+			display(play,word,mistakes);
+			if (play.getMistakes() == 6)
+			{
+				primaryStage.setScene(gameOver);
+				message.setText("you have lost");
+				play.reset();
+				display(play,word,mistakes);
+			} else if (!play.stillPlaying())
+			{
+				primaryStage.setScene(gameOver);
+				message.setText("you have won");
+				play.reset();
+				display(play,word,mistakes);
+			}
+		});
+		playButton.setOnAction(e -> {
+			primaryStage.setScene(playScene);
+		});
+
+		primaryStage.setScene(menuScene);
+		primaryStage.show();
+	}
+	
+	public static void display(Play p, Label l1, Label l2)
+	{
+		l1.setText(p.getWord());
+		l2.setText(p.getMistakes() + " mistakes");
+	}
+
+	public static void main(String[] args)
+	{
+		launch(args);
+	}
+}
---
+++ SourceCode/Play.java
@@ -0,0 +1,90 @@
+package hangman;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Play
+{
+	private static String word = "testing";
+	private static boolean[] letters = new boolean[word.length()];
+	private static int score;
+	private static int mistakes = 0;
+
+	public Play()
+	{
+		try
+		{
+			Random rand = new Random();
+			int n = rand.nextInt(6);
+			File file = new File("WordList.txt");
+			Scanner scan = new Scanner(file);
+			for (int i = 0; i <= n; i++)
+				word = scan.nextLine();
+			letters = new boolean[word.length()];
+			scan.close();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public boolean stillPlaying()
+	{
+		for (int i = 0; i < letters.length; i++)
+			if (letters[i] == false)
+				return true;
+		return false;
+
+	}
+
+	public void reset()
+	{
+		try
+		{
+			Random rand = new Random();
+			int n = rand.nextInt(6);
+			File file = new File("WordList.txt");
+			Scanner scan = new Scanner(file);
+			for (int i = 0; i <= n; i++)
+				word = scan.nextLine();
+			letters = new boolean[word.length()];
+			scan.close();
+			mistakes = 0;
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	public void play(char letter)
+	{
+		boolean correct = false;
+		for (int i = 0; i < word.length(); i++)
+			if (letter == word.charAt(i))
+			{
+				correct = true;
+				letters[i] = true;
+			}
+		if (!correct)
+			mistakes++;
+	}
+
+	public String getWord()
+	{
+		StringBuilder print = new StringBuilder();
+		for (int i = 0; i < letters.length; i++)
+			if (letters[i])
+				print.append(word.charAt(i) + " ");
+			else
+				print.append("_ ");
+		return print.toString();
+	}
+
+	public int getMistakes()
+	{
+		return mistakes;
+	}
+
+}
